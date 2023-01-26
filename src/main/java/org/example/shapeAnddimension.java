ccc2476a2ecdb78c18765cfddb4a18f21d8590e1
Tringapps-Ranjithkumar <ananthula.kumar@tringapps.com>
2023-01-26 16:18:08 +0530

    third commit

--- src/main/java/org/example/shapeAnddimension.java
+++ src/main/java/org/example/shapeAnddimension.java
@@ -2,7 +2,7 @@ package org.example;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-public class shapeS{
+public class shapeAnddimension{
     private static final Logger LOGGER =  Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     String typeOfShape;
     double length;
@@ -14,14 +14,14 @@ public class shapeS{
     double base;
     double height;
     static final float pI = 3.14f;
-    shapeS(float a,float b){
+    shapeAnddimension(float a,float b){
         length=a;
         breadth=b;
     }
-    shapeS(float b1,float h1,float s1,float s2,float s3){
+    shapeAnddimension(float b1,float h1,float s1,float s2,float s3){
         base=b1;height=h1;side1=s1;side2=s2;side3=s3;
     }
-    shapeS(float r){
+    shapeAnddimension(float r){
         radius=r;
     }
     public double perimeter(String s){
@@ -57,23 +57,24 @@ public class shapeS{
             float l=sc.nextFloat();
             LOGGER.log(Level.INFO,"Enter the breadth of the Rectangle");
             float b=sc.nextFloat();
-            LOGGER.log(Level.INFO,"Select the Below required option"+"\n"+"1.Perimeter"+"\n"+"2.Area"+"\n"+"3.Both");
+            String z=String.format("Select the Below required option\n1.Perimeter\n2.Area\n3.Both");
+            LOGGER.log(Level.INFO,z);
             int option=sc.nextInt();
             if(option==1){
-                shapeS figure=new shapeS(l,b);
+                shapeAnddimension figure=new shapeAnddimension(l,b);
                 double pm=figure.perimeter("rectangle");
                 LOGGER.log(Level.INFO,"The perimeter of the Rectangle is-----"+pm);
             }
             else if(option==2){
-                shapeS figure=new shapeS(l,b);
+                shapeAnddimension figure=new shapeAnddimension(l,b);
                 double ar=figure.area("rectangle");
                 LOGGER.log(Level.INFO,"The Area of the Rectangle is-----"+ar);
             }
             else if(option==3){
-                shapeS figure=new shapeS(l,b);
+                shapeAnddimension figure=new shapeAnddimension(l,b);
                 double pm=figure.perimeter("rectangle");
                 LOGGER.log(Level.INFO,"The perimeter of the Rectangle is-----"+pm);
-                shapeS figure1=new shapeS(l,b);
+                shapeAnddimension figure1=new shapeAnddimension(l,b);
                 double ar=figure1.area("rectangle");
                 LOGGER.log(Level.INFO,"The Area of the Rectangle is-----"+ar);
             }
@@ -90,23 +91,24 @@ public class shapeS{
             float s1=sc.nextFloat();
             float s2=sc.nextFloat();
             float s3=sc.nextFloat();
-            LOGGER.log(Level.INFO,"Select the Below required option"+"\n"+"1.Perimeter"+"\n"+"2.Area"+"\n"+"3.Both");
+            String z=String.format("Select the Below required option\n1.Perimeter\n2.Area\n3.Both");
+            LOGGER.log(Level.INFO,z);
             int option=sc.nextInt();
             if(option==1){
-                shapeS figure=new shapeS(b1,h1,s1,s2,s3);
+                shapeAnddimension figure=new shapeAnddimension(b1,h1,s1,s2,s3);
                 double pm1=figure.perimeter("Triangle");
                 LOGGER.log(Level.INFO,"The perimeter of the Triangle is-----"+pm1);
             }
             else if(option==2){
-                shapeS figure=new shapeS(b1,h1,s1,s2,s3);
+                shapeAnddimension figure=new shapeAnddimension(b1,h1,s1,s2,s3);
                 double ar1=figure.area("rectangle");
                 LOGGER.log(Level.INFO,"The Area of the Triangle is-----"+ar1);
             }
             else if(option==3){
-                shapeS figure=new shapeS(b1,h1,s1,s2,s3);
+                shapeAnddimension figure=new shapeAnddimension(b1,h1,s1,s2,s3);
                 double pm1=figure.perimeter("Triangle");
                 LOGGER.log(Level.INFO,"The perimeter of the Triangle is-----"+pm1);
-                shapeS figure2=new shapeS(b1,h1,s1,s2,s3);
+                shapeAnddimension figure2=new shapeAnddimension(b1,h1,s1,s2,s3);
                 double ar1=figure2.area("rectangle");
                 LOGGER.log(Level.INFO,"The Area of the Triangle is-----"+ar1);
             }
@@ -117,23 +119,24 @@ public class shapeS{
         else if((op.toUpperCase()).equals("CIRCLE")){
             LOGGER.log(Level.INFO,"Enter the radius of the Circle");
             float r=sc.nextFloat();
-            LOGGER.log(Level.INFO,"Select the Below required option"+"\n"+"1.Perimeter"+"\n"+"2.Area"+"\n"+"3.Both");
+            String z=String.format("Select the Below required option\n1.Perimeter\n2.Area\n3.Both");
+            LOGGER.log(Level.INFO,z);
             int option=sc.nextInt();
             if(option==1){
-                shapeS figure=new shapeS(r);
+                shapeAnddimension figure=new shapeAnddimension(r);
                 double pm2=figure.perimeter("circle");
                 LOGGER.log(Level.INFO,"The perimeter of the circle is-----"+pm2);
             }
             else if(option==2){
-                shapeS figure=new shapeS(r);
+                shapeAnddimension figure=new shapeAnddimension(r);
                 double ar2=figure.area("circle");
                 LOGGER.log(Level.INFO,"The Area of the circle is-----"+ar2);
             }
             else if(option==3){
-                shapeS figure=new shapeS(r);
+                shapeAnddimension figure=new shapeAnddimension(r);
                 double pm2=figure.perimeter("circle");
                 LOGGER.log(Level.INFO,"The perimeter of the circle is-----"+pm2);
-                shapeS figure3=new shapeS(r);
+                shapeAnddimension figure3=new shapeAnddimension(r);
                 double ar2=figure3.area("circle");
                 LOGGER.log(Level.INFO,"The Area of the circle is-----"+ar2);
             }
